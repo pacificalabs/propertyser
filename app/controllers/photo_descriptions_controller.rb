ae3fbbc1a4c86016f9c9cee4b4b3715c5084b90a
Tarun Mookhey <tarun@pacificasearch.com>
2024-09-13 13:33:00 +0100

    PHOTODescription now CRUDs correctly I promise

--- app/controllers/photo_descriptions_controller.rb
+++ app/controllers/photo_descriptions_controller.rb
@@ -1,15 +1,19 @@
 class PhotoDescriptionsController < ApplicationController
+  def create
+    p = PhotoDescription.create!(description_params)
+    flash.notice = "Your description has been saved!"
+    redirect_to photos_path(apartment_id: p.apartment.id)
+  end
+
   def update
     p = PhotoDescription.find(params[:id])
     p.update(description_params)
+    flash.notice = "Your description has been updated!"
     redirect_to photos_path(apartment_id: p.apartment.id)
   end
 
   private
   def description_params
-    params.require(:photo_description).permit(:description)
-  end
-  def apartment_params
-    params.permit(:apartment_id,:id)
+    params.require(:photo_description).permit(:description, :apartment_id, :blob_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   resources :apartments do
     resources :comments, only: [:create]
-    resources :photo_descriptions, only: [:update]
+    resources :photo_descriptions, only: [:create, :update]
     resources :market_scores, only: [:create]
     resources :floorplans, only: %i[create destroy update]
   end
