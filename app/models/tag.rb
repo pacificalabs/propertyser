e0c3fe32d6dde8d57a1c2dd96c4ff7560a772db4
Tarun Mookhey <tarun@pacificasearch.com>
2024-09-13 13:35:21 +0100

    schema updated

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  slug       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_tags_on_slug     (slug) UNIQUE
+#  index_tags_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 # app/models/tag.rb
 class Tag < ApplicationRecord
   extend FriendlyId
--- app/models/tag_relationship.rb
+++ app/models/tag_relationship.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: tag_relationships
+#
+#  id            :bigint           not null, primary key
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  child_tag_id  :bigint           not null
+#  parent_tag_id :bigint           not null
+#
+# Indexes
+#
+#  index_tag_relationships_on_child_tag_id                    (child_tag_id)
+#  index_tag_relationships_on_parent_tag_id                   (parent_tag_id)
+#  index_tag_relationships_on_parent_tag_id_and_child_tag_id  (parent_tag_id,child_tag_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (child_tag_id => tags.id)
+#  fk_rails_...  (parent_tag_id => tags.id)
+#
 # app/models/tag_relationship.rb
 class TagRelationship < ApplicationRecord
   belongs_to :parent_tag, class_name: 'Tag'
