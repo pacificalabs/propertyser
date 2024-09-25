3ba7275814f4f760f06924110009be3766cd5bbf
Tarun Mookhey <tarun@pacificasearch.com>
2024-09-25 16:50:01 +1000

    updated js file

--- app/assets/javascripts/floorplans.js
+++ app/assets/javascripts/floorplans.js
@@ -1,1 +1,12 @@
-function
+$(document).ready(function() {
+            // Size of browser viewport.
+            window.viewport = {
+                height: window.innerHeight,
+                width: window.innerWidth,
+                ratio: window.devicePixelRatio
+            }
+            document.cookie = "viewPort=" + JSON.stringify(window.viewport); -
+            $('form').on('submit', function() {
+                $('#progress-circle').removeClass('d-none').fadeIn();
+            });
+        }
